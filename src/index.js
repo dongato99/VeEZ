872435369467b0f53a3aae56b5627c06b06241b0
Arturo Aguilar <artrune@hotmail.com>
2021-06-22 19:57:07 -0500

    arreglando darkly

--- src/index.js
+++ src/index.js
@@ -1,51 +1,59 @@
-const {app, BrowserWindow, Menu} = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 const url = require('url');
 const path = require('path');
 const nodeConsole = require('console');
 
-if(process.env.NODE_ENV !== 'production'){
-require('electron-reload')(__dirname,{
-    electron: path.join(__dirname, '../node_modules','.bin','electron')
+if (process.env.NODE_ENV !== 'production') {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
 
-})
+    })
 }
 
 let mainWindow
 
-app.on('ready',() =>{
+app.on('ready', () => {
     mainWindow = new BrowserWindow({
+        show: false,
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
     })
-    mainWindow.maximize();
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'views/index.html'),
-        protocol: 'file',
-        slashes: true
-    }))
+    mainWindow.loadFile(path.join(__dirname, 'views/index.html'));
 
-    const mainMenu = Menu.buildFromTemplate(templateMenu);
-    Menu.setApplicationMenu(mainMenu);
+    //mainWindow.setMenuBarVisibility(false)
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(templateMenu));
+    mainWindow.webContents.on('did-finish-load', () => {
+        //mainWindow.maximize();
+        mainWindow.show()
+    })
 });
 
+
+
 const templateMenu = [
-    
+
 
 ]
 
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     templateMenu.push({
         label: 'DevTools',
-        submenu: [
-            {
+        submenu: [{
                 label: 'Show/Hide Dev Tools',
-                click(item,focusedWindow){
+                click(item, focusedWindow) {
                     focusedWindow.toggleDevTools();
                 }
             },
             {
-                role:'reload'
+                role: 'reload'
             }
         ]
     })
 }
-
